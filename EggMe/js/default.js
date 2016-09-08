7ec63a1ec6dbfc80ce90e596de4d3097e380fb82
MedaiP90 <bolzenxl@outlook.it>
2016-09-08 13:51:26 +0200

    Add files via upload

---
+++ EggMe/js/default.js
@@ -0,0 +1,54 @@
+﻿// Per un'introduzione al modello di navigazione, vedere la seguente documentazione:
+// http://go.microsoft.com/fwlink/?LinkID=329110
+(function () {
+    "use strict";
+
+    var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var nav = WinJS.Navigation;
+    var sched = WinJS.Utilities.Scheduler;
+    var ui = WinJS.UI;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: questa applicazione è stata appena avviata. Inizializzare
+                // l'applicazione qui.
+                /*document.getElementById("barn").addEventListener('click', function () {
+                    WinJS.Navigation.navigate("/pages/shop/barn.html");
+                }, false);
+                document.getElementById("shop").addEventListener('click', function () {
+                    WinJS.Navigation.navigate("/pages/shop/shop.html");
+                }, false);*/
+            } else {
+                // TODO: questa applicazione è stata sospesa e quindi terminata.
+                // Per creare un'esperienza utente uniforme, ripristinare qui lo stato dell'applicazione in modo che risulti come se l'app fosse sempre rimasta in esecuzione.
+            }
+
+            nav.history = app.sessionState.history || {};
+            nav.history.current.initialPlaceholder = true;
+
+            // Ottimizzare il carico dell'applicazione e, con la schermata iniziale visualizzata, eseguire il lavoro pianificato in alta priorità.
+            ui.disableAnimations();
+            var p = ui.processAll().then(function () {
+                return nav.navigate(nav.location || Application.navigator.home, nav.state);
+            }).then(function () {
+                return sched.requestDrain(sched.Priority.aboveNormal + 1);
+            }).then(function () {
+                ui.enableAnimations();
+            });
+
+            args.setPromise(p);
+        }
+    });
+
+    app.oncheckpoint = function (args) {
+        // TODO: questa applicazione sta per essere sospesa. Salvare qui eventuali stati
+        // che devono persistere attraverso le sospensioni. Se è necessario 
+        // completare un'operazione asincrona prima che l'applicazione 
+        // venga sospesa, chiamare args.setPromise().
+        app.sessionState.history = nav.history;
+    };
+
+    app.start();
+})();
---
+++ EggMe/js/navigator.js
@@ -0,0 +1,128 @@
+﻿(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+
+    WinJS.Namespace.define("Application", {
+        PageControlNavigator: WinJS.Class.define(
+            // Definire la funzione del costruttore per PageControlNavigator.
+            function PageControlNavigator(element, options) {
+                this._element = element || document.createElement("div");
+                this._element.appendChild(this._createPageElement());
+
+                this.home = options.home;
+
+                this._eventHandlerRemover = [];
+
+                var that = this;
+                function addRemovableEventListener(e, eventName, handler, capture) {
+                    e.addEventListener(eventName, handler, capture);
+                    that._eventHandlerRemover.push(function () {
+                        e.removeEventListener(eventName, handler);
+                    });
+                };
+
+                addRemovableEventListener(nav, 'navigating', this._navigating.bind(this), false);
+                addRemovableEventListener(nav, 'navigated', this._navigated.bind(this), false);
+
+                window.onresize = this._resized.bind(this);
+
+                Application.navigator = this;
+            }, {
+                home: "",
+                /// <campo domElement="true" />
+                _element: null,
+                _lastNavigationPromise: WinJS.Promise.as(),
+                _lastViewstate: 0,
+
+                // L'oggetto pagina attualmente caricato.
+                pageControl: {
+                    get: function () { return this.pageElement && this.pageElement.winControl; }
+                },
+
+                // Elemento radice della pagina corrente.
+                pageElement: {
+                    get: function () { return this._element.firstElementChild; }
+                },
+
+                // Questa funzione consente di eliminare la struttura delle pagine e relativo contenuto.
+                dispose: function () {
+                    if (this._disposed) {
+                        return;
+                    }
+
+                    this._disposed = true;
+                    WinJS.Utilities.disposeSubTree(this._element);
+                    for (var i = 0; i < this._eventHandlerRemover.length; i++) {
+                        this._eventHandlerRemover[i]();
+                    }
+                    this._eventHandlerRemover = null;
+                },
+
+                // Crea un contenitore in cui caricare una nuova pagina.
+                _createPageElement: function () {
+                    var element = document.createElement("div");
+                    element.setAttribute("dir", window.getComputedStyle(this._element, null).direction);
+                    element.style.position = "absolute";
+                    element.style.visibility = "hidden";
+                    element.style.width = "100%";
+                    element.style.height = "100%";
+                    return element;
+                },
+
+                // Recupera un elenco di elementi di animazione per la pagina corrente.
+                // Se la pagina non definisce un elenco, viene animata l'intera pagina.
+                _getAnimationElements: function () {
+                    if (this.pageControl && this.pageControl.getAnimationElements) {
+                        return this.pageControl.getAnimationElements();
+                    }
+                    return this.pageElement;
+                },
+
+                _navigated: function () {
+                    this.pageElement.style.visibility = "";
+                    WinJS.UI.Animation.enterPage(this._getAnimationElements()).done();
+                },
+
+                // Risponde alla navigazione aggiungendo nuove pagine al DOM. 
+                _navigating: function (args) {
+                    var newElement = this._createPageElement();
+                    this._element.appendChild(newElement);
+
+                    this._lastNavigationPromise.cancel();
+
+                    var that = this;
+
+                    function cleanup() {
+                        if (that._element.childElementCount > 1) {
+                            var oldElement = that._element.firstElementChild;
+                            // Pulire e rimuovere l'elemento precedente 
+                            if (oldElement.winControl) {
+                                if (oldElement.winControl.unload) {
+                                    oldElement.winControl.unload();
+                                }
+                                oldElement.winControl.dispose();
+                            }
+                            oldElement.parentNode.removeChild(oldElement);
+                            oldElement.innerText = "";
+                        }
+                    }
+
+                    this._lastNavigationPromise = WinJS.Promise.as().then(function () {
+                        return WinJS.UI.Pages.render(args.detail.location, newElement, args.detail.state);
+                    }).then(cleanup, cleanup);
+
+                    args.detail.setPromise(this._lastNavigationPromise);
+                },
+
+                // Risponde agli eventi di ridimensionamento e chiama la funzione updateLayout
+                // ella pagina caricata correntemente.
+                _resized: function (args) {
+                    if (this.pageControl && this.pageControl.updateLayout) {
+                        this.pageControl.updateLayout.call(this.pageControl, this.pageElement);
+                    }
+                },
+            }
+        )
+    });
+})();
